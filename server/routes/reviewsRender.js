93491fb6e591cd81822109903419cd881144e868
Dane Murphy <danem7@gmail.com>
2021-03-17 12:59:16 -0700

    SSR class handles the rendering of the react components

--- server/routes/reviewsRender.js
+++ server/routes/reviewsRender.js
@@ -1,47 +1,21 @@
 import fs from 'fs';
 import path from 'path';
-import axios from 'axios';
-
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import ReviewService from '../../services/reviewService/components/ReviewsContainer';
+import SSR from '../classes/SSR';
 
 module.exports = [
     '/buildings/:space',
     (req, res) => {
         fs.readFile(path.resolve(__dirname, '../../public/index.html'), 'utf8', async (err, data) => {
+            if (err) return res.status(500).json({ message: 'Internal server error' });
+
             try {
-                if (err) return res.status(500).json({ message: 'Internal server error' });
+                const ssr = new SSR(req.baseUrl, data);
+                const html = await ssr.getHtml();
 
-                const splitUrl = req.baseUrl.split('/');
-                const id = splitUrl[splitUrl.length - 1];
-
-                const resAll = await axios.get(`${process.env.REVIEWS_DOMAIN}/api/reviews/all/${id}`);
-                const html = ReactDOMServer.renderToString(
-                    <ReviewService
-                        exists={true}
-                        reviewInfo={resAll.data.reviewInfo}
-                        reviewsList={resAll.data.reviews}
-                    />,
-                );
-
-                const addReviews = data.replace('<div id="reviews"></div>', `<div id="reviews">${html}</div>`);
-                const addData = addReviews.replace(
-                    '<script id="global"></script>',
-                    `<script id="global">window.__initialData__ = ${JSON.stringify(resAll.data)}</script>`,
-                );
-
-                return res.send(addData);
+                return res.send(html);
             } catch (error) {
-                const noData = data.replace(
-                    '<script id="global"></script>',
-                    `<script id="global">window.__initialData__ = ${JSON.stringify({
-                        reviews: [],
-                        reviewInfo: { reviewCount: 0, avg: null },
-                    })} </script>`,
-                );
-
-                return res.send(noData);
+                const noDataHtml = SSR.getHtmlWithNoData(data);
+                return res.send(noDataHtml);
             }
         });
     },
