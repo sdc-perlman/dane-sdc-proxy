17c722c0f84723909e66a21fa40abf5698414046
Dane Murphy <danem7@gmail.com>
2021-03-18 13:13:49 -0700

    revising ssr to use bundle

--- server/index.js
+++ server/index.js
@@ -6,6 +6,4 @@ const port = process.env.PORT || 6002;
 
 app.use(...reviewsRender);
 
-app.get('/test', (req, res) => res.send('Ok!'));
-
 app.listen(port, () => console.log(`listening on port ${port}`));
