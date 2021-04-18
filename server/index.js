dc7fa59b91b3bbca09f881fcb1f0170ed2e1c083
Dane Murphy <danem7@gmail.com>
2021-04-18 14:23:58 -0700

    added morgan for logging

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 import renderRoute from './routes/render';
 
 const app = express();
 const port = process.env.PORT || 6002;
 
+app.use(morgan('common'));
 app.use(...renderRoute);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
